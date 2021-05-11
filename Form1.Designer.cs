2a95b8b6995c8c878a82011e14559d0f0011d0a2
NataliaSkorowska <nataliaskorowska@gmail.com>
2021-05-11 17:53:29 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace KonwerterCzasu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sekundy",
+            "Minuty",
+            "Godziny",
+            "Dni",
+            "Tygodnie",
+            "Miesiące",
+            "Lata"});
+            this.comboBox1.Location = new System.Drawing.Point(167, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Sekundy",
+            "Minuty",
+            "Godziny",
+            "Dni",
+            "Tygodnie",
+            "Miesiące",
+            "Lata"});
+            this.comboBox2.Location = new System.Drawing.Point(441, 174);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(181, 28);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(316, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 82);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Zamień jednostki czasu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 45);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "KONWERTER CZASU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(523, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "NA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(625, 37);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Chcę dokonać konwersji jednostek czasowych...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(326, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Wpisz liczbę:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,612 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonwerterCzasu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public double SekundyNaSekundy(double x)
+        {
+            return x;
+        }
+
+        /// <param name="x"></param>
+
+        public double MinutyNaSekundy(double x)
+        {
+            return x * 60;
+        }
+
+ 
+        /// <param name="x"></param>
+
+        public double GodzinyNaSekundy(double x)
+        {
+            return GodzinyNaMinuty(x) * 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaSekundy(double x)
+        {
+            return SekundyNaSekundy(GodzinyNaMinuty(DniNaGodziny(x))) * 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaSekundy(double x)
+        {
+            return x * 604800.02;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaSekundy(double x)
+        {
+            return 2629800 * x;
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaSekundy(double x)
+        {
+            return MiesiaceNaSekundy(x) * 12;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaMinuty(double x)
+        {
+            return x / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double MinutyNaMinuty(double x)
+        {
+            return x;
+        }
+
+        /// <param name="x"></param>
+
+        public double GodzinyNaMinuty(double x)
+        {
+            return x * 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaMinuty(double x)
+        {
+            return GodzinyNaMinuty(x) * 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaMinuty(double x)
+        {
+            return DniNaMinuty(x) * 7;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaMinuty(double x)
+        {
+            return 730.5 * 60 * x;
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaMinuty(double x)
+        {
+            return MiesiaceNaMinuty(x) * 12;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaGodziny(double x)
+        {
+            return x / 60 / 60;
+        }
+
+
+        /// <param name="x"></param>
+
+        public double MinutyNaGodziny(double x)
+        {
+            return x / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double GodzinyNaGodziny(double x)
+        {
+            return x;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaGodziny(double x)
+        {
+            return x * 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaGodziny(double x)
+        {
+            return DniNaGodziny(x) * 7;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaGodziny(double x)
+        {
+            return 30.4375 * x * 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaGodziny(double x)
+        {
+            return x * 365 * 24 + 6 * x;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaDni(double x)
+        {
+            return x * 1.15740;
+        }
+
+        /// <param name="x"></param>
+
+        public double MinutyNaDni(double x)
+        {
+            return GodzinyNaDni(x) / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double GodzinyNaDni(double x)
+        {
+            return x / 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaDni(double x)
+        {
+            return x;
+        }
+        /// <param name="x"></param>
+
+        public double TygodnieNaDni(double x)
+        {
+            return x * 7;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaDni(double x)
+        {
+            return 30.4375 * x;
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaDni(double x)
+        {
+            return MiesiaceNaDni(x) * 12;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaTygodnie(double x)
+        {
+            return MinutyNaTygodnie(x) / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double MinutyNaTygodnie(double x)
+        {
+            return GodzinyNaTygodnie(x) / 60;
+        }
+
+        /// <param name="x"></param>
+ 
+        public double GodzinyNaTygodnie(double x)
+        {
+            return DniNaTygodnie(x) / 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaTygodnie(double x)
+        {
+            return x * 0.14;
+        }
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaTygodnie(double x)
+        {
+            return x;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaTygodnie(double x)
+        {
+            double y = 4.35;
+            return Math.Floor(x * y);
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaTygodnie(double x)
+        {
+
+            return 52.18 * x;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaMiesiace(double x)
+        {
+            return MinutyNaMiesiace(x) / 60;
+        }
+
+
+        /// <param name="x"></param>
+
+        public double MinutyNaMiesiace(double x)
+        {
+            return GodzinyNaMiesiace(x) / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double GodzinyNaMiesiace(double x)
+        {
+            return DniNaMiesiace(x) / 24;
+        }
+
+        /// <param name="x"></param>
+
+        public double DniNaMiesiace(double x)
+        {
+            return x * 0.03;
+        }
+
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaMiesiace(double x)
+        {
+            return x * 0.23;
+        }
+
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaMiesiace(double x)
+        {
+            return x;
+        }
+
+
+        /// <param name="x"></param>
+
+        public double LataNaMiesiace(double x)
+        {
+
+            return x * 12;
+        }
+
+        /// <param name="x"></param>
+
+        public double SekundyNaLata(double x)
+        {
+            return MinutyNaLata(x) / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double MinutyNaLata(double x)
+        {
+            return GodzinyNaLata(x) / 60;
+        }
+
+        /// <param name="x"></param>
+
+        public double GodzinyNaLata(double x)
+        {
+            return DniNaLata(x) / 24;
+        }
+
+
+        /// <param name="x"></param>
+ 
+        public double DniNaLata(double x)
+        {
+            return x * 0.00274;
+        }
+
+        /// <param name="x"></param>
+
+        public double TygodnieNaLata(double x)
+        {
+            return x * 0.02;
+        }
+
+        /// <param name="x"></param>
+
+        public double MiesiaceNaLata(double x)
+        {
+            return x * 0.0833333333;
+        }
+
+        /// <param name="x"></param>
+
+        public double LataNaLata(double x)
+        {
+
+            return x;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+
+            if (textBox1.Text == "" || Convert.ToDouble(textBox1.Text) < 0)
+            {
+                MessageBox.Show("Należy wpisać liczbę, która będzie większa od zera");
+            }
+            else
+            {
+                try
+                {
+                    double x = Convert.ToDouble(textBox1.Text);
+
+                    if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = SekundyNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = SekundyNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = SekundyNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = SekundyNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = SekundyNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = SekundyNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 0 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = SekundyNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = MinutyNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = MinutyNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = MinutyNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = MinutyNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = MinutyNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = MinutyNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = MinutyNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = GodzinyNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = GodzinyNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = GodzinyNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = GodzinyNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = GodzinyNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = GodzinyNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = GodzinyNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = DniNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = DniNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = DniNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = DniNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = DniNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = DniNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 3 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = DniNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = TygodnieNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = TygodnieNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = TygodnieNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = TygodnieNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = TygodnieNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = TygodnieNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 4 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = TygodnieNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = MiesiaceNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = MiesiaceNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = MiesiaceNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = MiesiaceNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = MiesiaceNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = MiesiaceNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 5 && comboBox2.SelectedIndex == 6)
+                    {
+                        result = MiesiaceNaLata(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 0)
+                    {
+                        result = LataNaSekundy(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 1)
+                    {
+                        result = LataNaMinuty(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 2)
+                    {
+                        result = LataNaGodziny(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 3)
+                    {
+                        result = LataNaDni(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 4)
+                    {
+                        result = LataNaTygodnie(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 5)
+                    {
+                        result = LataNaMiesiace(x);
+                    }
+                    else if (comboBox1.SelectedIndex == 6 && comboBox2.SelectedIndex == 6)
+                    {
+                        result =LataNaLata(x);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wybierz jednostki czasu!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Wpisana wartość nie może być tekstem! ");
+                }
+            }
+            if (result != 0)
+            {
+                decimal d = Convert.ToDecimal(result);
+                if (d < 1)
+                    MessageBox.Show( "0" + d.ToString("#.##"));
+                else
+                    MessageBox.Show(d.ToString("#.##"));
+            }
+        
+        }
+    }
+}
